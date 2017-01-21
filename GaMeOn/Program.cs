f922e31e5f4ee21d12d9ee7038d69385e130c37d
Miikka Solonen <K9924@student.jamk.fi>
2017-01-21 12:51:50 +0200

    9kohtaan saakka

--- GaMeOn/Program.cs
+++ GaMeOn/Program.cs
@@ -16,7 +16,7 @@ namespace GaMeOn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GaMeOn());
         }
     }
 }
